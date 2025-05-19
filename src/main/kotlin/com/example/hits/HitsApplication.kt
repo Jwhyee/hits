5df490f7cdd9b1c88eefe770949e5cf81960ede1
JunYoung Ka <kawnsdud@naver.com>
2025-05-19 10:59:06 +0900

    feat : clear map every 00:00

--- src/main/kotlin/com/example/hits/HitsApplication.kt
+++ src/main/kotlin/com/example/hits/HitsApplication.kt
@@ -2,8 +2,10 @@ package com.example.hits
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class HitsApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/hits/domain/repository/HitRepository.kt
+++ src/main/kotlin/com/example/hits/domain/repository/HitRepository.kt
@@ -11,4 +11,8 @@ class HitRepository {
     fun increment(url: String): Int {
         return counter.computeIfAbsent(url) { AtomicInteger(0) }.incrementAndGet()
     }
+
+    fun resetAll() {
+        counter.clear()
+    }
 }
