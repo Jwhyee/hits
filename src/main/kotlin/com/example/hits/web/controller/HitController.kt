613fb305a5ea60cb92a66fec66f50786d36ebb56
Jwhyee <kawnsdud@naver.com>
2025-05-28 11:32:21 +0900

    refactor : HitsController

--- src/main/kotlin/com/example/hits/web/controller/HitController.kt
+++ src/main/kotlin/com/example/hits/web/controller/HitController.kt
@@ -8,9 +8,8 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("/api/count")
 class HitController(
     private val hitService: HitService
