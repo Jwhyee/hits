bf9ec46a5694a58c1454ca4891586dae60fae6a8
Jwhyee <kawnsdud@naver.com>
2025-05-28 11:25:17 +0900

    test : REST API test

--- src/test/kotlin/com/example/hits/controller/HitControllerTest.kt
+++ src/test/kotlin/com/example/hits/controller/HitControllerTest.kt
@@ -9,14 +9,17 @@ import io.kotest.matchers.string.shouldContain
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
-@WebMvcTest(HitController::class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @Import(HitControllerTest.TestConfig::class)
 class HitControllerTest : BehaviorSpec() {
 
